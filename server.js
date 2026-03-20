0c5e53879eb677b975deddb8625d20eb6d217527
DEV <josiel70c@gmail.com>
2026-03-20 20:10:57 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1888,7 +1888,7 @@ const MODEL_NAME = "gemini-2.5-flash"; // Atualizado para o modelo super rápido
 
             // --- ENVIO ---
 const LINK_CLOUDFLARE = "https://shawnee-unexamined-leanna.ngrok-free.dev"; // Colei seu ngrok aqui!
-const API_KEY_EVO = "sjs04ji5xlvzb0bujyx6b"; // Mantenha essa se for a mesma do seu Evolution
+const API_KEY_EVO = "Ja997640401"; // Mantenha essa se for a mesma do seu Evolution
 
             if (!respostaFinal) respostaFinal = "Erro interno.";
             respostaFinal = respostaFinal.replace(/undefined/g, "");
